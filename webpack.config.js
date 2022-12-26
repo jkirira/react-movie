096a3382901665b1df19388048d3c7a917611f75
jkirira <jkirira@cytonn.com>
2022-12-26 16:11:07 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'index.js',
+        clean: true,
+        assetModuleFilename: '[name][ext]',
+    },
+    devtool: 'source-map',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'public')
+        },
+        port: 3000,
+        open: false,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                },
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src/template.html'),
+            title: 'React ToDo',
+            filename: 'index.html',
+        })
+    ]
+}
