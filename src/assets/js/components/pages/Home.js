2cb658f139d5df9c974f38746ddd26546e36cba5
jkirira <jkirira@cytonn.com>
2023-01-02 12:30:17 +0300

    Adding Header Component

--- src/assets/js/components/pages/Home.js
+++ src/assets/js/components/pages/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "../partials/Header"
 
 export default function Home() {
 
     return (
         <>
+            <Header />
         </>
     );
 }
---
+++ src/assets/js/components/partials/Header.js
@@ -0,0 +1,41 @@
+import React, { useCallback, useMemo, useRef } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
+
+
+export default function Header() {
+
+    const navRef = useRef(null);
+
+    const toggleMenu = useCallback(() => {
+        let toggle_menu = navRef.current.getAttribute('data-toggle-menu') !== 'show' ? 'show' : 'hide';
+        navRef.current.setAttribute('data-toggle-menu', toggle_menu);
+    }, [])
+
+
+    const HamburgerIcon = useMemo(() => <FontAwesomeIcon icon={solid('bars')} onClick={toggleMenu} />, [toggleMenu])
+
+
+    return (
+        <div className="header">
+
+            <div className="icon">
+                Logo
+            </div>
+
+            
+            <div className="menu">
+                <div className="hamburger-icon">
+                    { HamburgerIcon }
+                </div>
+
+                <nav ref={navRef} data-toggle-menu="hide">
+                    <li>Profile</li>
+                    <li>Profile</li>
+                    <li>Profile</li>
+                </nav>
+            </div>
+
+        </div>
+    )
+}
