7619a26226125c55e5e2c0a6b34f37fc2e4b475c
jkirira <jkirira@cytonn.com>
2023-01-08 00:14:41 +0300

    Toggling responsive sidebar.

--- src/assets/js/components/partials/Sidebar.js
+++ src/assets/js/components/partials/Sidebar.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useMemo, useRef } from "react";
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 export default function Sidebar () {
+
+    const HamburgerIcon = useMemo(() => <FontAwesomeIcon icon={solid('bars')} />, []);
+
+    const sidebarRef = useRef(null);
+
+    const toggleSidebar = () => {
+        let toggle_sidebar = sidebarRef.current.getAttribute('data-toggle-sidebar') !== 'show' ? 'show' : 'hide';
+        sidebarRef.current.setAttribute('data-toggle-sidebar', toggle_sidebar);
+    }
+
     return (
-        <div className="sidebar">
+        <div className="sidebar" ref={sidebarRef}>
 
             <nav className="sidebar-nav">
                 <ul className="sidebar-nav-list">
@@ -15,6 +27,11 @@ export default function Sidebar () {
                 </ul>
             </nav>
 
+            <div className="sidebar-toggle" onClick={toggleSidebar}>
+                { HamburgerIcon }
+            </div>
+
         </div>
+
     );
 }
